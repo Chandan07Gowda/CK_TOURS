a3d5385dcc5951933d039269beb3fb742c070ec6
Chandan07Gowda <chandannallurkrishnappa@gmail.com>
2024-12-19 14:18:03 +0530

    Remove Unwanted

--- public/js/login.js
+++ public/js/login.js
@@ -36,16 +36,5 @@ document.querySelector('form').addEventListener('submit', (e) => {
   console.log(email, password);
   login(email, password); 
 });
-// export const logout = async () => {
-//   try {
-//     const res = await axios({
-//       method: 'GET',
-//       url: 'http://127.0.0.1:3000/api/v1/users/logout'
-//     });
-//     if ((res.data.status = 'success')) location.reload(true);
-//   } catch (err) {
-//     console.log(err.response);
-//     showAlert('error', 'Error logging out! Try again.');
-//   }
-// };
+
 
